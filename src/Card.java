a4e687196c811ff59eecacd41e55cd7b853f6582
BT_1N3_32@BKTECH.LOCAL <BT_1N3_32@BKTECH.LOCAL>
2018-12-10 10:11:59 -0500

    Initial commit

---
+++ src/Card.java
@@ -0,0 +1,18 @@
+public class Card {
+
+
+    private String rank;
+    private String suit;
+    private int pointValue;
+
+
+    public Card(String rank, String suit, int pointValue)
+    {
+        this.rank=rank;
+        this.suit=suit;
+        this.pointValue=pointValue;
+
+
+    }
+
+}
